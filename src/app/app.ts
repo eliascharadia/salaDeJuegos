ab75032e41ddd39f757dac90f8419342e8f3670c
eliascharadia <eliascharadia01@gmail.com>
2026-04-30 14:50:24 -0300

    commit de prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('salaDeJuegos');
+  protected readonly title = signal('Hola Amor');
 }
