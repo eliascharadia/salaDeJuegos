14a29a57ef419786c570ee224845f9e65552fcee
eliascharadia <eliascharadia01@gmail.com>
2026-05-06 15:04:29 -0300

    ADD: añadi ruta para el componenete quiensoy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Registro } from './components/registro/registro';
+import { QuienSoy } from './components/quien-soy/quien-soy';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
     {path: 'register', component: Registro},
+    {path: 'quiensoy', component: QuienSoy},
     { path: '**', redirectTo: 'login' }
 ];
