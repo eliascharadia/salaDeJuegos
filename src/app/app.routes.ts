f6a6a5e2e9d61ba0859e141913f9c2043188800c
eliascharadia <eliascharadia01@gmail.com>
2026-05-06 14:50:41 -0300

    add: path para el registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
+    {path: 'register', component: Registro},
     { path: '**', redirectTo: 'login' }
 ];
