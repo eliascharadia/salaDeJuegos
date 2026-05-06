07c8877f797ace3b6eca9cc42f9b3513d70a4980
eliascharadia <eliascharadia01@gmail.com>
2026-05-06 15:05:22 -0300

    Add: importaciones y añadido de botone para navegar a la ruta quien soy

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-CommonModule
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
