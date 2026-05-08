3eb1bf4ac728574be7ae9e548ec7d4dadca84ba1
eliascharadia <eliascharadia01@gmail.com>
2026-05-08 19:32:01 -0300

    probando el ignore

--- src/enviroments/enviroments.ts
+++
@@ -1,5 +0,0 @@
-export const enviroment = {
-    production: false,
-    supabaseUrl: 'https://wtcgytpeyfqpillgrrws.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0Y2d5dHBleWZxcGlsbGdycndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTI1NTEsImV4cCI6MjA5MzgyODU1MX0.VGjNdISZaLfKS-fpjIYEPIH35GyR7njXyMH1xBAOlnc'
-}
