5e757ea8c2b6761e1747575c8eb2faf8eee46bac
eliascharadia <eliascharadia01@gmail.com>
2026-05-08 19:08:21 -0300

    Agrega inviroment al .gitignore

---
+++ src/enviroments/enviroments.prod.ts
@@ -0,0 +1,5 @@
+export const enviroment = {
+    production: true,
+    supabaseUrl: 'https://wtcgytpeyfqpillgrrws.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0Y2d5dHBleWZxcGlsbGdycndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTI1NTEsImV4cCI6MjA5MzgyODU1MX0.VGjNdISZaLfKS-fpjIYEPIH35GyR7njXyMH1xBAOlnc'
+}
