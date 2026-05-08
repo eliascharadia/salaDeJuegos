0d3f227116307f82d178e0ea37ebb7f4c8e615de
eliascharadia <eliascharadia01@gmail.com>
2026-05-08 20:07:38 -0300

    ADD: servicio de supabase

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { enviroment } from '../../enviroments/enviroments.prod';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Supabase {}
+export class Supabase {
+  private client: SupabaseClient;
+
+  constructor(){
+    const supabaseURL = enviroment.supabaseUrl;
+    const supabaseKey = enviroment.supabaseKey;
+    this.client = createClient(supabaseURL, supabaseKey);
+  }
+
+  getClient(): SupabaseClient{
+    return this.client;
+  }
+}
