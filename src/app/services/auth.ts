407cc373048bf86a12cc8fee7d3ea0bad5dbc7e6
eliascharadia <eliascharadia01@gmail.com>
2026-05-09 20:51:56 -0300

    ADD: modelo usuario y auth service

---
+++ src/app/services/auth.ts
@@ -0,0 +1,86 @@
+import { Injectable, inject, signal, computed } from '@angular/core';
+import { Router } from '@angular/router';
+import { SupabaseService } from './supabase';
+import { UsuarioRegistro, Usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private router = inject(Router);
+  private supabase = inject(SupabaseService);
+
+  user = signal<Usuario | null>(null);
+  isAutheticated = computed(() => this.user() !== null);
+
+  constructor(){
+    this.getSession();
+  }
+
+  async getSession(){
+    const {data: {session}} = await this.supabase.getClient().auth.getSession();
+    if (session?.user){
+      this.user.set({
+        id: session.user.id,
+        email: session.user.email?? ''
+      })
+    }
+  }
+
+  async register(usuarioData: UsuarioRegistro): Promise<string | null> {
+
+    const { data, error } = await this.supabase.getClient().auth.signUp({
+      email: usuarioData.email,
+      password: usuarioData.password
+    });
+
+    if (error) {
+
+      if (error.message.includes('already registered')) {
+        return 'El usuario ya está registrado';
+      }
+
+      return 'Error al registrar usuario';
+    }
+
+    if (!data.user) {
+      return 'No se pudo crear el usuario';
+    }
+
+    const { error: dbError } = await this.supabase.getClient()
+      .from('usuarios')
+      .insert({
+        id: data.user.id,
+        nombre: usuarioData.nombre,
+        apellido: usuarioData.apellido,
+        edad: usuarioData.edad,
+        email: usuarioData.email
+      });
+
+    if (dbError) {
+      return 'Error al guardar datos del usuario';
+    }
+
+    // Login automático
+    await this.login(usuarioData.email, usuarioData.password);
+
+    // Navegar
+    this.router.navigate(['/home']);
+
+    return null;
+}
+
+async login(email: string, password: string) {
+  const {data, error} = await this.supabase.getClient().auth.signInWithPassword({email, password});
+  if(error) return false;
+
+  if(data.user){
+      this.user.set({id: data.user.id, email: data.user.email?? ''})
+      this.router.navigate(['/home']);
+      return true;
+  }
+  return false;
+}
+
+
+}
