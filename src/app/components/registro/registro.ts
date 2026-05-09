16474458c78f7c4873fcb32a9b0fb0559c282b4c
eliascharadia <eliascharadia01@gmail.com>
2026-05-09 20:54:14 -0300

    feat: hice la funcionalidad del componente registro

--- src/app/components/registro/registro.ts
+++ src/app/components/registro/registro.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Auth } from '../../services/auth';
+import { UsuarioRegistro } from '../../models/usuario';
+
 
 @Component({
   selector: 'app-registro',
@@ -9,12 +12,40 @@ import { RouterModule } from '@angular/router';
   styleUrl: './registro.css',
 })
 export class Registro {
+  private auth = inject(Auth);
+
   email =  '';
   password = '';
   confirmPassword = '';
   name = '';
+  lastName = '';
+  age = 0;
 
-  onSubmit(){
-
+  usuarioData: UsuarioRegistro = {
+    nombre: this.name,
+      apellido: this.lastName,
+      edad: this.age,
+      email: this.email,
+      password: this.password,
+  }
+  
+  loading = signal(false);
+  errorMensaje = signal<string | null>(null);
+  
+  async onSubmit(){
+    this.usuarioData = {
+      nombre: this.name,
+      apellido: this.lastName,
+      edad: this.age,
+      email: this.email,
+      password: this.password,
+    };
+    
+    this.loading.set(true);
+    const res = await this.auth.register(this.usuarioData);
+    if (res !== null){
+      this.errorMensaje.set(res)
+    }
+    this.loading.set(false);
   }
 }
