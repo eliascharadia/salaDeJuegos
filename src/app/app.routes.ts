6b93f476429ba6e2610f5931943bf3095d4005e3
eliascharadia <eliascharadia01@gmail.com>
2026-05-13 12:28:13 -0300

    add: añadi los componentes de los juegos y las rutas para navegar con el componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,24 @@ import { QuienSoy } from './components/quien-soy/quien-soy';
 import { BienvenidaHome } from './components/bienvenida-home/bienvenida-home';
 import { authGuard } from './guards/auth';
 import { Ahorcado } from './components/ahorcado/ahorcado';
+import { Mayoromenor } from './components/mayoromenor/mayoromenor';
+import { Preguntados } from './components/preguntados/preguntados';
 
 export const routes: Routes = [
-    { path: 'home', component: BienvenidaHome},
+    { path: 'home', component: BienvenidaHome },
     {
         path: '',
         component: BienvenidaHome,
         canActivate: [authGuard], // cuando haga el guard ponerlo aca
         children: [
-            {path: '', redirectTo: 'home', pathMatch: 'full'},
-            {path: 'ahorcado', component: Ahorcado}
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'ahorcado', component: Ahorcado },
+            { path: 'mayoromenor', component: Mayoromenor },
+            { path: 'preguntados', component: Preguntados }
         ]
     },
     { path: 'login', component: Login },
-    {path: 'register', component: Registro},
-    {path: 'quiensoy', component: QuienSoy},
+    { path: 'register', component: Registro },
+    { path: 'quiensoy', component: QuienSoy },
     { path: '**', redirectTo: 'home' }
 ];
