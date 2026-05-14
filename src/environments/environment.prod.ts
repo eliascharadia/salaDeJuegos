647771f6ab714266bce057d3b61d1f6b6728c753
Elías Damián Charadia <89148679+eliascharadia@users.noreply.github.com>
2026-05-14 19:56:36 -0300

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    supabaseUrl: 'https://shlfhopdbcmdavbzfqzd.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNobGZob3BkYmNtZGF2YnpmcXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNzE3MjYsImV4cCI6MjA5Mzg0NzcyNn0.NbBuDoDNVJmaGrzoUK4wIVRW4TJTqZvhxATvF8pCtY8'
-}
